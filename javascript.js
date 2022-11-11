6aa11f694cb7200832d10b8cf6ae9882f25d045b
Robert De Lappe <robertgdel@gmail.com>
2022-11-11 13:38:12 -0800

    Add other data type randomization

--- javascript.js
+++ javascript.js
@@ -24,10 +24,17 @@ let setValues = () =>
     dataType = document.querySelector("input[name='data-type']:checked").id;
 
     size = parseInt(sizeElement.value);
-    steps = parseInt(stepsElement.value);
-    min = parseInt(minRangeElement.value);
+    steps = parseFloat(stepsElement.value);
+    min = parseFloat(minRangeElement.value);
     if (maxRangeElement.value == null) max = (min + 2);
     else max = maxRangeElement.value;
+}
+
+
+function randomChar()
+{
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    return characters.charAt(Math.floor(Math.random() * characters.length));
 }
 
 function generateArray()
@@ -38,20 +45,47 @@ function generateArray()
 
     for (let i = 0; i < size; i++)
     {
-        let increment;
-        
-        if (arrayType === "linear") increment = (i * steps + min);
-        if (arrayType === "random")
+        if (dataType == "int" || dataType == "double")
         {
-            increment = parseInt((Math.random() * (max - min) + min));
+            let increment = 0.0;
+        
+            if (arrayType === "linear")
+            {
+                increment = (i * steps + min);
+            }
+    
+            if (arrayType === "random")
+            {
+                increment = ((Math.random() * (max - min) + min));
+            }
+    
+            increment = Math.round(increment * 100) / 100;
+            if (dataType == "int") increment = parseInt(increment);
+    
+            output += increment;
         }
+        else if (dataType == "String")
+        {
+            let result = "";
+            let length = Math.random() * (max - min) + min;
 
-        output += increment;
+            for (let j = 0; j < length; j++ ) 
+            {
+                result += randomChar();
+            }
+
+            output += '"' + result + '"';
+        }
+        else
+        {
+            output += "'" + randomChar() + "'";
+        }
 
         if (i < size - 1) output += ", ";
     }
 
     outputElement.innerHTML = output + " };";
 }
+
 
 generateArray();
